f579cf32ec46c07f182034b48452b0707c6e51fc
brucemi <bruce.mitchell@derivco.co.za>
2018-04-24 17:49:39 +0200

    add redux react and webpack

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,37 @@
+let webpack = require('webpack'),
+    path = require('path'),
+    parentDir = path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: path.resolve(__dirname, '/node_modules/'),
+                loader: 'babel-loader',
+                options: {
+                    plugins: 'transform-object-rest-spread'
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'less-loader'}
+                ]
+            }
+        ],
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+};
